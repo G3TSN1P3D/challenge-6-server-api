3e9d6c90b3d787d7e362a5efde5f68a9da036a87
G3TSN1P3D <chava4flores@hotmail.com>
2022-05-18 09:46:31 -0400

    Latest update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -32,7 +32,6 @@ function GetWeatherApi(lat, lon) {
       let tempK = data.current.temp;
       uvIndex = data.current.uvi;
       weatherIcon = data.current.weather[0].icon;
-      let iconUrl = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
 
       temp = Math.trunc(((tempK - 273.15) * 9) / 5 + 32);
     })
@@ -44,16 +43,18 @@ function getDate() {
   dateObject = new Date(milliseconds).toDateString();
 }
 function getWeatherData() {
+  document.querySelector(".current-weather").setAttribute("style", "visibility: visible");
+  document.querySelector("#five-day-forecast").setAttribute("style", "visibility: visible");
   getDate();
   iconE.src = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
   h2Title.textContent = `${cityName.value} (${dateObject})`;
 
   h2Title.appendChild(iconE);
 
-  weatherTemp.textContent = `${temp} F\u00B0`;
-  weatherWind.textContent = `${wind} MPH`;
-  weatherHumidity.textContent = `${humidity}%`;
-  weatherUv.textContent = uvIndex;
+  weatherTemp.textContent = `Temp: ${temp} F\u00B0`;
+  weatherWind.textContent = `Wind: ${wind} MPH`;
+  weatherHumidity.textContent = `Humidity: ${humidity}%`;
+  weatherUv.textContent = `UV Index: ${uvIndex}`;
 }
 function GetCityApi() {
   let requestUrlCity = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName.value}&limit=1&appid=${apiKey}`;
